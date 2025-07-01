749572b9280eecc4a3e355c39a82964ff00925e5
Rubey4112 <74166553+Rubey4112@users.noreply.github.com>
2025-07-01 01:50:20 -0400

    Got code to compiled.

--- reactor_code/reactor_code.ino
+++ reactor_code/reactor_code.ino
@@ -15,10 +15,19 @@ const int LED1 = D0;
 const int LED2 = D1;
 const int LED3 = D10;
 const int LED4 = D9;
-const int[] led_array = [LED1, LED2, LED3, LED4];
+const int led_array[4] = {LED1, LED2, LED3, LED4};
 
 bool start = false;
 int difficulty = 0; // 0-9
+
+typedef struct {
+  int indicator; // indicate the button the player must press to move on
+  long prev_time;
+  int duration;
+}RoundData;
+
+RoundData round_data = {0, 0, 0};
+
 
 void setup() {
   button1.attach(BUT1, INPUT_PULLUP);
@@ -41,7 +50,7 @@ void setup() {
   pinMode(LED3, OUTPUT);
   pinMode(LED4, OUTPUT);
 
-  randomSeed();
+  randomSeed(analogRead(A0));
 }
 
 /*
@@ -59,13 +68,13 @@ Buttons layout
 4 change game mode
 */
 void game_menu() {
-  if (button1 && button4) {
+  if (button1.pressed() && button4.pressed()) {
     start = true;
   }
-  if (button2) {
+  if (button2.pressed()) {
     difficulty--;
   }
-  if (button3) {
+  if (button3.pressed()) {
     difficulty++;
   }
 }
@@ -82,17 +91,35 @@ every level the difficulty increase
 delay decreases slowly but eventually get fast and then slow again
 using a precalculated table
 */
-const int[10] delay_table = [800, 760, 720, 668, 611, 540, 400, 325, 320, 300]
-int get_delay(int difficulty) {
-  return difficulty < 10 ? delay_table[difficulty] : delay_table[9];
+const int duration_table[10] = {800, 760, 720, 668, 611, 540, 400, 325, 320, 300};
+int get_duration(int difficulty) {
+  return difficulty < 10 ? duration_table[difficulty] : duration_table[9];
 }
 
+int get_delay() {
+  return int(random(200, 1600));
+}
+
+
+/* 
+This function should only be called once at the start of every round
+*/
+void new_round(bool has_run) {
+  if (!has_run) {
+    new_round_helper();
+    has_run = true;
+  }
+}
+
+void new_round_helper() {
+  
+}
 
 bool loose = false;
 int level = 0;
 bool lights_on = false;
 
-Bounce2::Button[4] button_array = [button1, button2, button3, button4];
+Bounce2::Button button_array[4] = {button1, button2, button3, button4};
 
 /* 
 return the round the player reaches
@@ -100,23 +127,23 @@ return the round the player reaches
 random amount of time before showing player the lights
 */
 void game_loop() {
-  int indicator = int(random(4));
-  long prev_time = millis();
+
   long curr_time = millis();
-  int delay = get_delay(difficulty);
+
+  
   if (!lights_on) {
     lights_on = true;
-    digitalWrite(indicator, HIGH);
+    digitalWrite(led_array[round_data.indicator], HIGH);
   }
-  if (!loose && curr_time - prev_time < delay)
+  if (!loose && curr_time - round_data.prev_time < round_data.duration)
   {
     for (int i = 0; i < 4; i++) 
     {
       if (button_array[i].pressed()) {
-        if (indicator) {
+        if (i == round_data.indicator) {
           difficulty++;
           level++;
-          digitalWrite(indicator, LOW);
+          digitalWrite(round_data.indicator, LOW);
         }
         else {
           loose = true;
@@ -137,12 +164,32 @@ void game_loose() {
   loose = false;
   difficulty = 0;
 
+  for (int i = 0; i < 3; i++) {
+    digitalWrite(LED1, HIGH);
+    digitalWrite(LED2, HIGH);
+    digitalWrite(LED3, HIGH);
+    digitalWrite(LED4, HIGH);
+    delay(200);
+    digitalWrite(LED1, LOW);
+    digitalWrite(LED2, LOW);
+    digitalWrite(LED3, LOW);
+    digitalWrite(LED4, LOW);
+  }
+
   for (int i = level % 10; i >= 0; i--) {
-    digitalWrite(LED1, )
+    digitalWrite(LED1, HIGH);
+    digitalWrite(LED2, HIGH);
+    delay(350);
+    digitalWrite(LED1, LOW);
+    digitalWrite(LED2, LOW);
   }
 
   for (int i = level / 10; i >= 0; i--) {
-    
+    digitalWrite(LED3, HIGH);
+    digitalWrite(LED4, HIGH);
+    delay(350);
+    digitalWrite(LED3, LOW);
+    digitalWrite(LED4, LOW);
   }
 
   level = 0;
