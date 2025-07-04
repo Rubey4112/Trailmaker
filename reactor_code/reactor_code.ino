a9f6e140515895800fa46f60c614c6634a74f539
Rubey4112 <74166553+Rubey4112@users.noreply.github.com>
2025-07-04 15:29:07 -0400

    Code bug fixing and PCB finalization
    Added silkscreen to PCB and exported gerber and dril file for JLC PCB

--- reactor_code/reactor_code.ino
+++ reactor_code/reactor_code.ino
@@ -5,12 +5,9 @@ const int BUT1 = D2;
 const int BUT2 = D3;
 const int BUT3 = D7;
 const int BUT4 = D8;
+const int button_pin_array[4] = {BUT1, BUT2, BUT3, BUT4};
 
-Bounce2::Button button1 = Bounce2::Button();
-Bounce2::Button button2 = Bounce2::Button();
-Bounce2::Button button3 = Bounce2::Button();
-Bounce2::Button button4 = Bounce2::Button();
-
+Bounce2::Button* button_array = new Bounce2::Button[4];
 
 const int LED1 = D0;
 const int LED2 = D1;
@@ -20,6 +17,10 @@ const int led_array[4] = {LED1, LED2, LED3, LED4};
 
 bool start = false;
 int difficulty = 0; // 0-9
+bool loose = false;
+int level = 0;
+bool lights_on = false;
+
 
 typedef struct {
   int indicator; // indicate the button the player must press to move on
@@ -34,20 +35,12 @@ RoundData round_data = {0, 0, 0};
 
 
 void setup() {
-  button1.attach(BUT1, INPUT_PULLUP);
-  button2.attach(BUT2, INPUT_PULLUP);
-  button3.attach(BUT3, INPUT_PULLUP);
-  button4.attach(BUT4, INPUT_PULLUP);
-
-  button1.interval(5);
-  button2.interval(5);
-  button3.interval(5);
-  button4.interval(5);
-
-  button1.setPressedState(LOW);
-  button2.setPressedState(LOW);
-  button3.setPressedState(LOW);
-  button4.setPressedState(LOW);
+  Serial1.begin(9600);
+  for (int i = 0; i < 4; i++) {
+    button_array[i].attach(button_pin_array[i], INPUT_PULLUP);
+    button_array[i].interval(5);
+    button_array[i].setPressedState(LOW);
+  }
 
   pinMode(LED1, OUTPUT);
   pinMode(LED2, OUTPUT);
@@ -58,42 +51,43 @@ void setup() {
 }
 
 /*
-Buttons layout
-1 2 3 4
- */
+  Buttons layout
+  1 2 3 4
+*/
 
 
-/* 
-1 + 4 = Start game
-2 decrease difficulty
-3 increase difficulty
+/*
+  1 + 4 = Start game
+  2 decrease difficulty
+  3 increase difficulty
 
-1 view previous high score
-4 change game mode
+  1 view previous high score
+  4 change game mode
 */
 void game_menu() {
-  if (button1.pressed() && button4.pressed()) {
+  Serial1.println("menu");
+  if (button_array[0].isPressed() && button_array[3].isPressed()) {
     start = true;
   }
-  if (button2.pressed()) {
+  if (button_array[1].pressed()) {
     difficulty--;
   }
-  if (button3.pressed()) {
+  if (button_array[2].pressed()) {
     difficulty++;
   }
 }
 
 
-/* 
-if button pressed and indicator on -> next round
-if button pressed and indicator off -> you loose
-if timer ran out -> you loose
-every level the difficulty increase
+/*
+  if button pressed and indicator on -> next round
+  if button pressed and indicator off -> you loose
+  if timer ran out -> you loose
+  every level the difficulty increase
 */
 
-/* 
-delay decreases slowly but eventually get fast and then slow again
-using a precalculated table
+/*
+  delay decreases slowly but eventually get fast and then slow again
+  using a precalculated table
 */
 const int duration_table[10] = {800, 760, 720, 668, 611, 540, 400, 325, 320, 300};
 int get_duration(int difficulty) {
@@ -105,8 +99,8 @@ int get_delay() {
 }
 
 
-/* 
-This function should only be called once at the start of every round
+/*
+  This function should only be called once at the start of every round
 */
 void new_round(int difficulty, bool has_run) {
   if (!has_run) {
@@ -119,29 +113,23 @@ void new_round_helper(int difficulty) {
   round_data = {random(4), millis(), get_duration(difficulty)};
 }
 
-bool loose = false;
-int level = 0;
-bool lights_on = false;
+/*
+  return the round the player reaches
 
-Bounce2::Button button_array[4] = {button1, button2, button3, button4};
-
-/* 
-return the round the player reaches
-
-random amount of time before showing player the lights
+  random amount of time before showing player the lights
 */
 void game_loop() {
+  Serial1.println("game loop");
+  unsigned long curr_time = millis();
 
-   unsigned long curr_time = millis();
 
-  
   if (!lights_on) {
     lights_on = true;
     digitalWrite(led_array[round_data.indicator], HIGH);
   }
   if (!loose && curr_time - round_data.prev_time < round_data.duration)
   {
-    for (int i = 0; i < 4; i++) 
+    for (int i = 0; i < 4; i++)
     {
       if (button_array[i].pressed()) {
         if (i == round_data.indicator) {
@@ -154,17 +142,18 @@ void game_loop() {
         }
       }
     }
-  } 
+  }
   else {
     game_loose();
   }
 }
 
-/* 
-reset global val and display score
+/*
+  reset global val and display score
 */
 void game_loose() {
-  start = false;
+  Serial1.println("game loose");
+  // start = false;
   loose = false;
   difficulty = 0;
 
@@ -173,12 +162,14 @@ void game_loose() {
     digitalWrite(LED2, HIGH);
     digitalWrite(LED3, HIGH);
     digitalWrite(LED4, HIGH);
-    delay(200);
+    delay(500);
     digitalWrite(LED1, LOW);
     digitalWrite(LED2, LOW);
     digitalWrite(LED3, LOW);
     digitalWrite(LED4, LOW);
+    delay(500);
   }
+  delay(500);
 
   for (int i = level % 10; i >= 0; i--) {
     digitalWrite(LED1, HIGH);
@@ -186,6 +177,7 @@ void game_loose() {
     delay(350);
     digitalWrite(LED1, LOW);
     digitalWrite(LED2, LOW);
+    delay(350);
   }
 
   for (int i = level / 10; i >= 0; i--) {
@@ -194,16 +186,17 @@ void game_loose() {
     delay(350);
     digitalWrite(LED3, LOW);
     digitalWrite(LED4, LOW);
+    delay(350);
   }
 
   level = 0;
-  
+
 
 }
 
 void test_mode() {
-  for(int i = 0; i<4; i++) {
-    if(button_array[i].pressed()) {
+  for (int i = 0; i < 4; i++) {
+    if (button_array[i].isPressed()) {
       digitalWrite(led_array[i], HIGH);
     }
     else {
@@ -212,17 +205,36 @@ void test_mode() {
   }
 }
 
-void loop() {
-  button1.update();
-  button2.update();
-  button3.update();
-  button4.update();
+void led_test_mode() {
+  for (int i = 0; i < 4; i++) {
+    digitalWrite(led_array[i], HIGH);
+    delay(1000);
+    digitalWrite(led_array[i], LOW);
+    delay(1000);
+  }
+}
 
-  #ifdef TESTMODE
-    test_mode();
-  #endif
+void update_buttons() {
+  for (int i = 0; i < 4; i++) {
+    button_array[i].update();
+  }
+}
+
+void loop() {
+  Serial1.println(start);
+  update_buttons();
+  delay(1); // for wokwi simulation so it doesn't lag
+
+
+#ifdef TESTMODE
+  test_mode();
+#endif
 
   #ifndef TESTMODE
-    start ? game_loop() : game_menu();
+    if (start) {
+      game_loop();
+    } else {
+      game_menu();
+    }
   #endif
 }
